c55b648460d0186a4508270b03c891e1a288b388
grussell06 <grussell06@icloud.com>
2026-04-03 00:00:31 -0400

    added signup.php

--- php/signup.php
+++ php/signup.php
@@ -8,26 +8,10 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>PanLove</title>
+    <title>Sign Up</title>
   </head>
   <body>
 
-
-<!--Login form-->
-    <h1> Welcome to PanLove </h1>
-    <p>Enter your email</p>
-<form action="./login.php" method="POST"> 
-    <label for="email">Email:</label><br>
-    <input type="email" id="email" name="email" required><br><br>
-    <p>Enter your password</p>
-    <label for="password">Password:</label><br>
-    <input type="password" id="password" name="password" required><br><br>
-    <button type="submit">Login</button>
-</form>
-
-<!--Take to Signup form-->
-    <h1>Don't have an account?</h1>
-    <button onclick="window.location.href='./signup.php'">Sign Up</button>
 
 
 
